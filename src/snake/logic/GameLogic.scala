ba00d0b755e0ecfd84397812554f202f9ddfc58c
CobaltKrayt <georgecovalciuc00@gmail.com>
2023-09-11 19:43:58 +0200

    Snake is functional if a bit ugly. The apple spawning is funny. Refactor.

--- src/snake/logic/GameLogic.scala
+++ src/snake/logic/GameLogic.scala
@@ -11,14 +11,25 @@ import snake.logic.GameLogic._
 class GameLogic(val random: RandomGenerator,
                 val gridDims: Dimensions) {
 
-  def gameOver: Boolean = false
+  def gameOver: Boolean = snake.tail.exists(segment => segment._1 == nextPosition)
 
-  var snakeBody: List[Point] = List(Point(5, 3))
-  var currentPosition = Point(5, 3)
+  var currentPosition = Point(2, 0)
   var currentDirection: Direction = East()
-  var nextPosition = Point(0, 0)
+  var nextPosition = Point(2, 0)
+
+
+  var snake: List[(Point, CellType)] = List((Point(2, 0), SnakeHead(East())), (Point(1, 0), SnakeBody()), (Point(0, 0), SnakeBody()))
+
+  val allPoints: IndexedSeq[Point] =
+    for {
+      x <- 0 until gridDims.width
+      y <- 0 until gridDims.height
+    } yield Point(x, y)
+
+  var freeSpots: List[Point] = allPoints.toList
+
+
   var currentApple = Point(random.randomInt(gridDims.width), random.randomInt(gridDims.height))
-  var snakeLentgh = 0
 
   // TODO implement me
   def step(): Unit = {
@@ -35,12 +46,28 @@ class GameLogic(val random: RandomGenerator,
     if (nextPosition.y > gridDims.height - 1) nextPosition = Point(nextPosition.x, 0)
     if (nextPosition.y < 0) nextPosition = Point(nextPosition.x, gridDims.height - 1)
 
-    snakeBody = nextPosition +: snakeBody
+    if (gameOver) return
+
+    val newHead = (nextPosition, SnakeHead(currentDirection))
+
+    val increment = 1.0f / snake.length
+
+    val updatedBody = snake.map {
+
+      case (point, SnakeHead(_))
+      => (point, SnakeBody(increment))
+
+      case (point, SnakeBody(distance))
+      => (point, SnakeBody(distance + increment))
+
+    }
 
     if (nextPosition == currentApple) {
-      snakeLentgh += 1
+      snake = newHead +: updatedBody
       currentApple = Point(random.randomInt(gridDims.width), random.randomInt(gridDims.height))
-    } else snakeBody = snakeBody.init
+    } else {
+      snake = newHead +: updatedBody.init
+    }
 
     currentPosition = nextPosition
   }
@@ -57,10 +84,11 @@ class GameLogic(val random: RandomGenerator,
 
   // TODO implement me
   def getCellType(p: Point): CellType = {
-    if (p == currentPosition) SnakeHead(currentDirection)
-    else if (snakeBody.contains(p)) SnakeBody()
-    else if (p == currentApple) Apple()
-    else Empty()
+    if (p == currentApple) {
+      Apple()
+    } else {
+      snake.find(segment => segment._1 == p).map(segment => segment._2).getOrElse(Empty())
+    }
   }
 
   // TODO implement me
@@ -71,7 +99,7 @@ class GameLogic(val random: RandomGenerator,
 /** GameLogic companion object */
 object GameLogic {
 
-  val FramesPerSecond: Int = 5 // change this to increase/decrease speed of game
+  val FramesPerSecond: Int = 8 // change this to increase/decrease speed of game
 
   val DrawSizeFactor = 1.5 // increase this to make the game bigger (for high-res screens)
   // or decrease to make game smaller
